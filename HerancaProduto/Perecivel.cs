39d3c90d2e77b5bfc69b319bd448c2ae7464c746
pedrohcornetti <pedro.cornetti@fatec.sp.gov.br>
2024-04-29 19:39:59 -0300

    Herança

--- HerancaProduto/Perecivel.cs
+++ HerancaProduto/Perecivel.cs
@@ -5,8 +5,26 @@ using System.Threading.Tasks;
 
 namespace HerancaProduto
 {
-    public class Perecivel
+    public class Perecivel : Produto
     {
+       private string dataFabricacao;
+       private string dataValidade;
+       private int lote;
+       public string DataFabricacao
+       {
+        get { return dataFabricacao; }
+        set { dataFabricacao = value; }
+       }
+         public string DataValidade
+         {
+          get { return dataValidade; }
+          set { dataValidade = value; }
+         }
+        public int Lote
+        {
+        get { return lote; }
+        set { lote = value; }
+        }
         
     }
 }
--- HerancaProduto/Produto.cs
+++ HerancaProduto/Produto.cs
@@ -7,6 +7,24 @@ namespace HerancaProduto
 {
     public class Produto
     {
+        protected int codigo;
+        protected string nome;
+        protected double preco;
         
+        public int Codigo
+        {
+            get {return codigo;}
+            set { codigo = value;}
+        }
+        public string Nome
+        {
+            get {return nome;}
+            set { nome = value;}
+        }
+        public double Preco
+        {
+            get {return preco;}
+            set { preco = value;}
+        }
     }
 }
