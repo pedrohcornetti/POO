dede52660a855bb51068cab3f00d21ab0d70b263
pedrohcornetti <pedrocornetti@outlook.com>
2024-05-13 20:40:18 -0300

    teste

---
+++ AbstratoCliente/Teste.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstratoCliente
+{
+    public class Teste
+    {
+        public void verificaIdade(Cliente cliente)
+        {
+            cliente.AvalaIdade();
+        }
+
+    }
+
+}
