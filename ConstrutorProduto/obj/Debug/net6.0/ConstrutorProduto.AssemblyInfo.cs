d07ddac9a5f7f54ef766ea31d952df062795274b
pedrohcornetti <pedrocornetti@outlook.com>
2024-05-27 20:04:31 -0300

    InterfaceAuteticavel

--- ConstrutorProduto/obj/Debug/net6.0/ConstrutorProduto.AssemblyInfo.cs
+++ ConstrutorProduto/obj/Debug/net6.0/ConstrutorProduto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConstrutorProduto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d3c90d2e77b5bfc69b319bd448c2ae7464c746")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConstrutorProduto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConstrutorProduto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- InterfaceAutenticavel/obj/Debug/net6.0/InterfaceAutenticavel.AssemblyInfo.cs
+++ InterfaceAutenticavel/obj/Debug/net6.0/InterfaceAutenticavel.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("EncapsulamentoConta")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("InterfaceAutenticavel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("EncapsulamentoConta")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EncapsulamentoConta")]
+[assembly: System.Reflection.AssemblyProductAttribute("InterfaceAutenticavel")]
+[assembly: System.Reflection.AssemblyTitleAttribute("InterfaceAutenticavel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
